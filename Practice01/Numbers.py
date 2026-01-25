d3f787bffca8dce1fc60d99aae06edf775fc12c2
SerikbaevaArailym <aserikbaeva776@gmail.com>
2026-01-25 20:45:43 +0500

    Finish task 4 in practice01

---
+++ Practice01/Numbers.py
@@ -0,0 +1,25 @@
+#1
+x = float(1)
+y = int(2.8)
+z = complex(1)
+print(x)
+print(y)
+print(z)
+print(type(x))
+print(type(y))
+print(type(z))
+
+#2
+a = 10+2
+
+#3
+b = 20*10
+
+#4
+c=20/2
+
+#5
+d=7%3
+
+#6
+l=50-10
