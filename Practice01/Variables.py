18fb058716d80f07f81cbbbb5b1966880f0ad97a
SerikbaevaArailym <aserikbaeva776@gmail.com>
2026-01-25 20:27:26 +0500

    Finish task 4 in practice01

---
+++ Practice01/Variables.py
@@ -0,0 +1,30 @@
+#1
+a=4
+b=5
+print(a)
+print(b)
+
+#2
+age=17 #integer
+name="Asyl" #string
+print(age)
+print(name)
+
+#3
+m=1
+n=2
+c=3
+sum=m+n+c
+print(sum)
+
+#4
+x=4
+x="Salem"
+print(x)
+
+#5
+f="Aruzhan"
+w=25
+print(f, w)
+
+
