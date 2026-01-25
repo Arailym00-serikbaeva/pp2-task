eba7af66f031f465d471a99bd312f2f54acb685a
SerikbaevaArailym <aserikbaeva776@gmail.com>
2026-01-25 19:31:45 +0500

    Finish task 3 in practice01

---
+++ Practice01/Syntax.py
@@ -0,0 +1,29 @@
+#1
+a=20
+name = "Arailym"
+pi=3.14
+print(a)
+print(name)
+print(pi)
+
+#2
+x=18
+if x>=18:
+    print("You are an adult")
+else:
+    print("You are a minor")
+
+#3
+def add(x,y):
+    return x+y
+res=add(10,20)
+print(res)
+
+#4
+for i in range(1,5):
+    print(i)
+
+#5
+numbers=[0,2,4,6,8]
+for n in numbers:
+    print(n)
