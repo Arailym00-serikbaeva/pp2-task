141797b136334a17af2666fa5869aecfd4ec9986
SerikbaevaArailym <aserikbaeva776@gmail.com>
2026-01-25 21:06:59 +0500

    Finish task 9 in practice01

---
+++ Practice01/String.py
@@ -0,0 +1,19 @@
+#1
+a = "Python"
+print(a[0], a[-1])
+
+#2
+b = "Hello, World!"
+print(len(b))
+
+#3
+s = "hello"
+print(s.upper()) 
+
+#4
+c="awesome"
+print(c[3:])
+
+#5
+x = 'Welcome'
+print(x[2:6])
