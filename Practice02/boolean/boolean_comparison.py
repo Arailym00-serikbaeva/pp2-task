ff9f995566f0a48ebf8a38bb769d75a59957ec38
SerikbaevaArailym <aserikbaeva776@gmail.com>
2026-02-04 02:10:01 +0500

    Add Practice02

---
+++ Practice02/boolean/boolean_comparison.py
@@ -0,0 +1,19 @@
+#1
+print(10>=10) #True
+
+#2
+print((5+5)==(20/2)) #True
+
+#3
+print("Apple" == "apple") #False
+
+#4
+x=10
+y=20
+n=x<y
+print(f"Is {x} less than {y}? {n}") #Is 10 less than 20? True
+
+#5
+list1=[1,2,3]
+list2=[4,5]
+print(len(list1)>len(list2)) #True
