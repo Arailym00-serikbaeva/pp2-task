bddded2f7b98aafac7ab8e5d7916d395a86a1bb1
Arailym00-serikbaeva <aserikbaeva776@gmail.com>
2026-02-28 14:23:05 +0500

    Update generators.py

--- Practice04/generators.py
+++ Practice04/generators.py
@@ -4,11 +4,29 @@ iterator = iter(numbers)
 print(next(iterator)) 
 print(next(iterator)) 
 
+n=int(input())
+for i in range(1,n+1):
+    print(i**2)
+    
 #2
 numbers = [10, 20, 30]
 iterator = iter(numbers)
 for num in iterator:
     print(num)
+
+
+def even_numbers(n):
+    for i in range(0, n + 1, 2):
+        yield i
+n = int(input().strip())
+first = True
+for num in even_numbers(n):
+    if not first:
+        print(",", end="")
+    print(num, end="")
+    first = False
+
+
 
 #3
 class MyNumbers:
@@ -27,6 +45,19 @@ print(next(myiter))
 print(next(myiter))
 print(next(myiter))
 
+def divisible_numbers(n):
+    for i in range(0, n + 1):
+        if i % 3 == 0 and i % 4 == 0:
+            yield i
+n = int(input().strip())
+first = True
+for num in divisible_numbers(n):
+    if not first:
+        print(" ", end="")
+    print(num, end="")
+    first = False
+    
+
 #4
 def count_up_to(max_value):
     current = 1
@@ -36,7 +67,15 @@ def count_up_to(max_value):
 for number in count_up_to(4):
     print(number)
 
+a,b=map(int,input().split())
+for i in range(a,b+1):
+    print(i**2)
+
 #5
 squares = (x * x for x in range(5))
 for square in squares:
     print(square)
+
+n=int(input())
+for i in range(-n,1):
+    print(-i)
