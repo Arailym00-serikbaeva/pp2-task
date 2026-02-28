535b67037ceb71f7ea2ecfa8a19b7689ccb8c30a
Arailym00-serikbaeva <aserikbaeva776@gmail.com>
2026-02-28 15:09:27 +0500

    Update json_tasks.py

--- Practice04/json_tasks.py
+++ Practice04/json_tasks.py
@@ -4,6 +4,8 @@ x =  '{ "name":"Aisha", "age":18, "city":"Shymkent"}'
 y = json.loads(x)
 print(y["age"])
 
+
+
 #2
 import json
 x = {
@@ -46,3 +48,27 @@ with open("product.json", "r") as file:
     data = json.load(file)
 print(data)
 print(data["product"])
+
+
+
+
+
+import json
+with open("sample-data.json") as file:
+    data = json.load(file)
+
+print("Interface Status")
+print("=" * 80)
+
+print(f"{'DN':50} {'Description':20} {'Speed':8} {'MTU':6}")
+print("-" * 80)
+
+for item in data["imdata"]:
+    attributes = item["l1PhysIf"]["attributes"]
+
+    dn = attributes.get("dn", "")
+    descr = attributes.get("descr", "")
+    speed = attributes.get("speed", "")
+    mtu = attributes.get("mtu", "")
+
+    print(f"{dn:50} {descr:20} {speed:8} {mtu:6}")
