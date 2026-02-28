de55afb4042257ce77abff9fe94a7c48a49023e0
Arailym00-serikbaeva <aserikbaeva776@gmail.com>
2026-02-28 14:18:31 +0500

    Update dates.py

--- Practice04/dates.py
+++ Practice04/dates.py
@@ -3,21 +3,44 @@ import datetime
 a = datetime.datetime.now()
 print(a)
 
+import datetime
+x=datetime.datetime.now()
+n=x-datetime.timedelta(days=5)
+print(n)
+
 #2
 import datetime
 a = datetime.datetime.now()
 print(a.year)
 print(a.strftime("%A"))
 
+import datetime
+x=datetime.datetime.now()
+n=x-datetime.timedelta(days=1)
+print(n)
+a=x+datetime.timedelta(days=1)
+print(a)
+
 #3
 import datetime
 x = datetime.datetime(2026, 2, 27)
 print(x.strftime('%d'))
 
+import datetime
+a = datetime.datetime.now()
+print(a.replace(microsecond=0))
+
+
 #4
 import datetime
 x = datetime.datetime(2026, 2, 27)
 print(x.strftime("%B"))
+
+import datetime
+x = datetime.datetime(2026, 2, 27,14,58,45)
+y= datetime.datetime(2026, 3, 29,11,40,23)
+a=y-x
+print(a.total_seconds())
 
 #5
 import datetime
